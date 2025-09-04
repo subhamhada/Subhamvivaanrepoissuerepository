6ff4b9e7d1d819b97f5982dbbf6fff744943b4d5
subhamhada <subhamrao0011@gmail.com>
2025-09-04 11:17:53 +0530

    Add files via upload

---
+++ stack_push.py
@@ -0,0 +1,18 @@
+# Simple stack push example
+
+# Initial empty stack
+stack = []
+
+print("Initial stack:", stack)
+
+# Push elements onto the stack
+stack.append(10)
+print("Pushed: 10")
+
+stack.append(20)
+print("Pushed: 20")
+
+stack.append(30)
+print("Pushed: 30")
+
+print("Stack after pushing:", stack)
